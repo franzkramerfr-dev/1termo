ba831b5baa402f6379507cbabaa6b03df3dcc16d
Franz Kramer da Silva <franzkramerfr@gmail.com>
2026-05-16 20:31:58 -0300

    Atualizando Pastas

---
+++ Python/Aula 1/primeiro.py
@@ -0,0 +1,30 @@
+# ===Aula 1===
+
+# Instalação de Aplicativos:
+
+# -Visual Code
+#code.visualstudio.com
+
+# -Notion (Sem instalador)
+# Meu caderno de anotações
+
+#Python
+# Linguagem de Programação
+# Instalador e compilador da programação
+# https://www.python.org/
+
+# Instalador Windows para compilar os códigos em Python
+# Instalação da IDE Python
+# Criando Perfil Python no Visual Code
+# Extensão Python no Visual Code
+
+#Comandos Python:
+# print: Exibir mensagem;
+# input: Receber dados do usuário;
+# int: Converter um valor para inteiro;
+# float: Converter um valor para decimal;
+# str: Converter um valor para string;
+# Variáveis: Armazenar dados para uso posterior;
+# Comentários: Explicar o código para outros programadores ou para si mesmo no futuro;
+# Operadores Aritméticos: Realizar operações matemáticas (soma, subtração, multiplicação, divisão);
+
